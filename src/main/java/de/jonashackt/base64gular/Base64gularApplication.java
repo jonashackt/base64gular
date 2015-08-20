5ef698d48a8a1613c13c958ec367252ae20d53bc
Jonas Hecht <jonas.hecht@codecentric.de>
2015-08-20 11:15:06 +0200

    initial commit

---
+++ src/main/java/de/jonashackt/base64gular/Base64gularApplication.java
@@ -0,0 +1,12 @@
+package de.jonashackt.base64gular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Base64gularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Base64gularApplication.class, args);
+    }
+}
---
+++ src/test/java/de/jonashackt/base64gular/Base64gularApplicationTests.java
@@ -0,0 +1,18 @@
+package de.jonashackt.base64gular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Base64gularApplication.class)
+@WebAppConfiguration
+public class Base64gularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
