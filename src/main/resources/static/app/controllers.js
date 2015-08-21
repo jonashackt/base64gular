0f01b77ad513c001138a74ca5d3521f22309f31e
Jonas Hecht <jonas.hecht@codecentric.de>
2015-08-21 15:50:39 +0200

    Hmm, it works - but now without using the actual REST-Endpoints, cause there´s a javascript-function to decode base64-stuff... Arghl

--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -10,6 +10,10 @@ base64gular.controller('Base64gularCtrl', ['PdfService', function(PdfService) {
     ctrl.decodedPdf = [];
 
     ctrl.convertPdf = function() {
-        ctrl.decodedPdf = PdfService.convertPdf(ctrl.pdf);
+        window.open("data:application/pdf;base64, " + ctrl.pdf.base64endcodedPdf);
+
+        //ctrl.decodedPdf = PdfService.convertPdf(ctrl.pdf);
+
+        //window.open("data:application/pdf;byte[]," + ctrl.decodedPdf,'_blank','height=600,width=400');
     };
 }]);
